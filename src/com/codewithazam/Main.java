69fc68b464c98f2fc2e3963cad80346d6f337a80
Azamkhuja Ibragimov <ibragimov.azamkhuja.2020@gmail.com>
2022-07-22 13:44:28 -0400

    Selenium Review01

---
+++ src/com/codewithazam/Main.java
@@ -0,0 +1,9 @@
+package com.codewithazam;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/com/codewithazam/review01/NavigateCommands.java
@@ -0,0 +1,40 @@
+package com.codewithazam.review01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateCommands {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        // driver.get("https://www.signal-telecom.net/store/");
+        // will wait for the elements to be loaded
+
+        driver.navigate().to("https://www.signal-telecom.net/store/");
+        // will NOT wait for the elements to be loaded
+        // will keep the history
+        Thread.sleep(1000);
+
+        driver.navigate().to("https://www.amazon.com/");
+        Thread.sleep(2000);
+
+        driver.navigate().refresh();
+        Thread.sleep(1000);
+
+        driver.navigate().back();
+        // will go back to the previous page
+
+        driver.navigate().forward();
+        Thread.sleep(1000);
+
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+
+        driver.manage().window().minimize();
+        Thread.sleep(1000);
+
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
